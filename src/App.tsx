686f959f107d1b8a407c97e2a6e16140a387051f
Ol-Wood <98314947+Ol-Wood@users.noreply.github.com>
2023-10-27 17:17:36 +0100

    Added shadow to banner and made it larger on smaller screens

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Banner from "./components/Banner";
 
 function App() {
   return (
-    <div className="pt-3 pb-10">
+    <>
       <Banner />
-      <div className="px-[5%] lg:px-[10%] mt-10 flex flex-col gap-y-6">
-        <Home />
+      <div className="pb-10">
+        <div className="px-[5%] lg:px-[10%] mt-10 flex flex-col gap-y-6">
+          <Home />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import Contact from "../assets/phone.svg?react";
 
 export default function Banner() {
   return (
-    <div className="lg:h-auto h-32 pb-3 border-b-4 border-gray px-[5%] lg:px-[10%]">
+    <div className="lg:h-auto h-40 py-3 shadow-[0_4px_10px_-2px_white] px-[5%] lg:px-[10%]">
       <div className="xl:max-w-[1300px] lg:max-w-[900px] flex m-auto items-center justify-between h-full">
         <Logo className="h-full lg:h-[3rem] w-auto" />
         <div className="basis-1/2 flex gap-x-20 lg:gap-x-6 justify-end pt-2">
